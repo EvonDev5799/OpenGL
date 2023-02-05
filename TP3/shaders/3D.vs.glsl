f663bf14fa2e36f4582d1dd2758ad82ba982b894
EvonDev5799 <evondev5799@gmail.com>
2023-02-05 13:35:36 +0100

    sphere functionnal

--- TP3/shaders/3D.vs.glsl
+++ TP3/shaders/3D.vs.glsl
@@ -1,20 +1,30 @@
 #version 330 core
 
-layout(location = 0) in vec3 aVertexPosition;
-layout(location = 1) in vec3 aVertexNormal;
-layout(location = 2) in vec3 aVertexTexCoord;
+// Attributs de sommet
+layout(location = 0) in vec3 aVertexPosition; // Position du sommet
+layout(location = 1) in vec3 aVertexNormal; // Normale du sommet
+layout(location = 2) in vec2 aVertexTexCoords; // Coordonnées de texture du sommet
 
-uniform mat4 uMVPMatrix, uMVMatrix et uNormalMatrix;
+// Matrices de transformations reçues en uniform
+uniform mat4 uMVPMatrix;
+uniform mat4 uMVMatrix;
+uniform mat4 uNormalMatrix;
 
-out vec2 vViewVertexNormal;
-out vec2 vViewVertexPosition;
-out vec2 vUV;
-
-out vec2 vVertexPosition;
-out vec2 vVertexNormal;
-out vec2 vVertexTexCoord;
+// Sorties du shader
+out vec3 vPosition_vs; // Position du sommet transformé dans l'espace View
+out vec3 vNormal_vs; // Normale du sommet transformé dans l'espace View
+out vec2 vTexCoords; // Coordonnées de texture du sommet
 
 void main() {
-    vVertexTexCoord = aVertexTexCoord;
-    vVertexPosition = uMVMatrix * aVertexPosition
-};
+    // Passage en coordonnées homogènes
+    vec4 vertexPosition = vec4(aVertexPosition, 1);
+    vec4 vertexNormal = vec4(aVertexNormal, 0);
+
+    // Calcul des valeurs de sortie
+    vPosition_vs = vec3(uMVMatrix * vertexPosition);
+    vNormal_vs = vec3(uNormalMatrix * vertexNormal);
+    vTexCoords = aVertexTexCoords;
+
+    // Calcul de la position projetée
+    gl_Position = uMVPMatrix * vertexPosition;
+}
---
+++ TP3/sphere.cpp
@@ -0,0 +1,118 @@
+#include <glimac/SDLWindowManager.hpp>
+#include <GL/glew.h>
+#include <iostream>
+#include <glimac/common.hpp>
+#include <glimac/Sphere.hpp>
+#include <glimac/Program.hpp>
+#include <vector>
+
+using namespace glimac;
+
+struct WindowParameters {
+
+    uint32_t width;
+    uint32_t height;
+    const char* title;
+
+    WindowParameters(uint32_t width, uint32_t height, const char* title) : width(width), height(height), title(title) {}
+};
+
+int main(int argc, char **argv)
+{
+    WindowParameters window_params = WindowParameters(800, 600, "GLImac");
+
+    // Initialize SDL and open a window
+    SDLWindowManager windowManager(window_params.height, window_params.width, window_params.title);
+
+    // Initialize glew for OpenGL3+ support
+    GLenum glewInitError = glewInit();
+    if (GLEW_OK != glewInitError)
+    {
+        std::cerr << glewGetErrorString(glewInitError) << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    std::cout << "OpenGL Version : " << glGetString(GL_VERSION) << std::endl;
+    std::cout << "GLEW Version : " << glewGetString(GLEW_VERSION) << std::endl;
+
+    /*********************************
+     * HERE SHOULD COME THE INITIALIZATION CODE
+     *********************************/
+
+    Sphere s = Sphere(1, 10, 10);
+
+    GLuint vbo;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(ShapeVertex) * s.getVertexCount(), s.getDataPointer(), GL_STATIC_DRAW);
+
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    const GLuint VERTEX_ATTR_POSITION = 0;
+    const GLuint VERTEX_ATTR_NORMAL = 1;
+    const GLuint VERTEX_ATTR_TEX_COORD = 2;
+
+    glEnableVertexAttribArray(VERTEX_ATTR_POSITION);
+    glEnableVertexAttribArray(VERTEX_ATTR_NORMAL);
+    glEnableVertexAttribArray(VERTEX_ATTR_TEX_COORD);
+
+    glVertexAttribPointer(VERTEX_ATTR_POSITION, 3, GL_FLOAT, GL_FALSE, sizeof(ShapeVertex), (void *)offsetof(ShapeVertex, position));
+    glVertexAttribPointer(VERTEX_ATTR_NORMAL, 3, GL_FLOAT, GL_FALSE, sizeof(ShapeVertex), (void *)offsetof(ShapeVertex, normal));
+    glVertexAttribPointer(VERTEX_ATTR_TEX_COORD, 3, GL_FLOAT, GL_FALSE, sizeof(ShapeVertex), (void *)offsetof(ShapeVertex, texCoords));
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    FilePath applicationPath(argv[0]);
+    Program program = loadProgram(applicationPath.dirPath() + "shaders/3D.vs.glsl",
+                                  applicationPath.dirPath() + "shaders/normals.fs.glsl");
+    program.use();
+
+    GLint uMVPMatrix = glGetUniformLocation(program.getGLId(), "uMVPMatrix");
+    GLint uMVMatrix = glGetUniformLocation(program.getGLId(), "uMVMatrix");
+    GLint uNormalMatrix = glGetUniformLocation(program.getGLId(), "uNormalMatrix");
+
+    glEnable(GL_DEPTH_TEST);
+
+    glm::mat4 ProjMatrix, MVMatrix, NormalMatrix;
+    ProjMatrix = glm::perspective(glm::radians(70.f), (float) window_params.height / window_params.width, 0.1f, 100.f);
+    MVMatrix = glm::translate(glm::mat4(1.0f),glm::vec3(0, 0, -5));
+    NormalMatrix = glm::transpose(glm::inverse(MVMatrix));
+
+    // Application loop:
+    bool done = false;
+    while (!done)
+    {
+        // Event loop:
+        SDL_Event e;
+        while (windowManager.pollEvent(e))
+        {
+            if (e.type == SDL_QUIT)
+            {
+                done = true; // Leave the loop after this iteration
+            }
+        }
+
+        /*********************************
+         * HERE SHOULD COME THE RENDERING CODE
+         *********************************/
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glBindVertexArray(vao);
+        glUniformMatrix4fv(uMVPMatrix, 1, GL_FALSE, glm::value_ptr(ProjMatrix * MVMatrix));
+        glUniformMatrix4fv(uMVMatrix, 1, GL_FALSE, glm::value_ptr(MVMatrix));
+        glUniformMatrix4fv(uNormalMatrix, 1, GL_FALSE, glm::value_ptr(NormalMatrix));
+
+        glDrawArrays(GL_TRIANGLES, 0, s.getVertexCount());
+
+        glBindVertexArray(0);
+
+        // Update the display
+        windowManager.swapBuffers();
+    }
+
+    return EXIT_SUCCESS;
+}
